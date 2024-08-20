d19530cb170d294c13b57b8ef6cd257a4e1821f6
Nawras20 <66115245+Nawras20@users.noreply.github.com>
2024-08-20 11:36:31 +0200

    Edit Todo added.

--- resources/js/script.js
+++ resources/js/script.js
@@ -5,17 +5,33 @@ function documentLoaded() {
     showTasks();
 }
 
+const editModal = document.getElementById('editModal')
+if (editModal) {
+    editModal.addEventListener('show.bs.modal', event => {
+        const button = event.relatedTarget
+
+        const id = button.getAttribute('id');
+        const btnsave = document.getElementById('btnsave');
+        btnsave.setAttribute("onClick", "editTask(" + id + ")");
+
+        const todos = getLocalStorage();
+        const todo = todos[id].title;
+        const modalBodyInput = editModal.querySelector('.modal-body textarea')
+        modalBodyInput.value = todo
+    })
+}
+
 function createNewArrayInLocalStorage() {
     if (getLocalStorage() === null) {
         setLocalStorage(new Array());
     }
 }
 
-function setLocalStorage(value){
+function setLocalStorage(value) {
     localStorage.setItem("todos", JSON.stringify(value));
 }
 
-function getLocalStorage(){
+function getLocalStorage() {
     return JSON.parse(localStorage.getItem("todos"));
 }
 
@@ -31,10 +47,11 @@ function showTasks() {
         tasklist.appendChild(div);
 
         let li = document.createElement("li");
-        li.setAttribute("class", "list-group-item d-flex justify-content-between align-items-center");
+        li.setAttribute("class", "row list-group-item d-flex justify-content-between align-items-center");
         div.appendChild(li);
 
         let container = document.createElement("div");
+        container.setAttribute("class", "col-lg-9");
         li.appendChild(container);
 
         let input = document.createElement("input");
@@ -53,12 +70,25 @@ function showTasks() {
         label.textContent = todo.title;
         container.appendChild(label);
 
+        let buttons = document.createElement("div");
+        buttons.setAttribute("class", "col-lg-3");
+        li.appendChild(buttons);
+
+        let btnEdit = document.createElement("button");
+        btnEdit.type = "button";
+        btnEdit.setAttribute("id", index);
+        btnEdit.setAttribute("class", "btn btn-outline-secondary btn-sm btns");
+        btnEdit.setAttribute("data-bs-toggle", "modal");
+        btnEdit.setAttribute("data-bs-target", "#editModal");
+        btnEdit.textContent = "Bearbeiten";
+        buttons.appendChild(btnEdit);
+
         let btn = document.createElement("button");
         btn.type = "button";
-        btn.setAttribute("class", "btn btn-outline-danger");
+        btn.setAttribute("class", "btn btn-outline-danger btn-sm btns");
         btn.setAttribute("onclick", "deleteTask(" + index + ")");
         btn.textContent = "Löschen";
-        li.appendChild(btn);
+        buttons.appendChild(btn);
     });
 }
 
@@ -113,3 +143,15 @@ function checkboxChanged(e) {
 
     setLocalStorage(todos);
 }
+
+function editTask(index) {
+    const todos = getLocalStorage();
+
+    const editModal = document.getElementById('editModal');
+    const modalBodyInput = editModal.querySelector('.modal-body textarea')
+    todos[index].title = modalBodyInput.value;
+    
+    setLocalStorage(todos);
+    showTasks();
+}
+
